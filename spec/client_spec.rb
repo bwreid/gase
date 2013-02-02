2c48ba99d384b8d884fbdc13f7a9cf93d292b1dd
Bryan W. Reid <bwreid@gmail.com>
2013-02-02 15:31:30 -0500

    Pushing sample doc

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,15 +1,16 @@
 require_relative 'spec_helper'
-require_relative '../gase'
+require_relative '../portfolio'
+require_relative '../client'
 
 describe Client do
-  let(:client) {Client.new('Joe')}
+  let(:client) {Client.new('Joe', 400_000)}
 
   describe ".new" do
     it "create client" do
       expect(client).to_not eq nil
     end
     it "has no portfolios" do
-      expect(client.portfolio.count).to eq 0
+      expect(client.portfolios.count).to eq 0
     end
   end
   #test client exists
@@ -23,16 +24,18 @@ describe Client do
 
   describe "#create_portfolio" do
   it "create a portfolio" do
-    client.create_portfolio('AARP', 500)
-    expect(client.portfolio['Joe']).to eq 500
+    client.create_portfolio('TeChnOlogy')
+    puts client.portfolios
+    expect(client.portfolios[:technology].name).to eq "Technology"
     end
   end
 
   describe "#balance" do
-   it "test" do
-    portfolio.create_portfolio('AARP', 500)
-    portfolio.balance('AARP', 500)
-    expect(portfolio.balance['AARP']).to eq 500
+   it "check to make sure client's portfolio does indeed equal 0 " do
+    client.create_portfolio('AARP')
+    client.portfolios[:aarp].balance = 500
+    puts client.portfolios
+    expect(client.portfolios[:aarp].balance).to eq 500
     end
   end
 end
